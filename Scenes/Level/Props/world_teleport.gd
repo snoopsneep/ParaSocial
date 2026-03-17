9dbe51f02d1b185be15370d5c24220a5d73d2f3e
snoopsneep <41456831+snoopsneep@users.noreply.github.com>
2026-03-17 06:04:43 -0400

    cleaning up duplicated scenes folder pt1
    man i gotta get better at using godot lmao

--- Scenes/Level/Props/world_teleport.gd
+++
@@ -1,6 +0,0 @@
-class_name WorldTeleport extends WorldEvent
-
-@export var destination: Vector2
-
-func run_event(manager: EventManager):
-	pass
--- Scenes/Managers/event_manager.gd
+++
@@ -1,1 +0,0 @@
-class_name EventManager extends Node
